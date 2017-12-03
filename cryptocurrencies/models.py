60f8a180b62ff804368c56153ec1dc8acafa1055
Jean-Philippe Bidegain <jp@bidega.in>
2017-12-03 21:23:44 +0100

    Besic portfolio display

--- cryptocurrencies/models.py
+++ cryptocurrencies/models.py
@@ -7,3 +7,14 @@ class CryptoCurrency(models.Model):
     balance = models.DecimalField(max_digits=19, decimal_places=10)
     price = MoneyField(max_digits=10, decimal_places=2, default_currency='EUR')
     spent = MoneyField(max_digits=10, decimal_places=2, default_currency='EUR')
+
+    def total(self):
+        return self.balance * self.price
+
+
+    def gain(self):
+        return (self.balance * self.price) - self.spent
+
+
+    def __str__(self):
+        return self.name
--- cryptocurrencies/views.py
+++ cryptocurrencies/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import CryptoCurrency
 
-# Create your views here.
+
+def index(request):
+    crypto_currencies = CryptoCurrency.objects.all()
+    template = loader.get_template('cryptocurrencies/index.html')
+    context = {
+        'crypto_currencies': crypto_currencies,
+    }
+    return HttpResponse(template.render(context, request))
